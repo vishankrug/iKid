a8075a7a26a8eee608a1bbdc5f0e456f73cb0afd
Vishank Rughwani <vishankr@uw.edu>
2021-05-04 23:51:29 -0700

    app

--- iKid/ViewController.swift
+++ iKid/ViewController.swift
@@ -7,13 +7,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoodViewController: UIViewController {
+    
+    @IBOutlet var GoodLabel: UILabel!
+    @IBOutlet var GoodButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GoodLabel.text = "What did the gorilla call the donkey"
+    }
+    
+    @IBAction func GoodButton(_ sender: UIButton) {
+        GoodLabel.text = "donkey"
+    }
+    
+    
+}
+
+class PunViewController: UIViewController {
+    
+    @IBOutlet var PunLabel: UILabel!
+    @IBOutlet var PunButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        PunLabel.text = "What did the gorilla call the donkey"
     }
 
 
+   @IBAction func PunButton(_ sender: UIButton) {
+        present(PunPunchViewController(), animated: true)
+    }
+    
 }
+
+class PunPunchViewController: UIViewController {
+    
+    @IBOutlet var PunPunchLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        //PunPunchLabel.text = ""
+    }
+
+    
+}
+
+
+
+class DadViewController: UIViewController {
+    
+    @IBOutlet var DadLabel: UILabel!
+    @IBOutlet var DadButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        DadLabel.text = "What did the gorilla call the donkey"
+    }
+    
+    @IBAction func DadButton(_ sender: UIButton) {
+        DadLabel.text = "Hi"
+    }
+    
+    
+    
+}
+
+
 
