497dca48c1b1f92578792c9e0e03d9732d2ea929
Vishank Rughwani <vishankr@uw.edu>
2021-05-05 01:50:12 -0700

    working animations

--- iKid/ViewController.swift
+++ iKid/ViewController.swift
@@ -11,16 +11,40 @@ class GoodViewController: UIViewController {
     
     @IBOutlet var GoodLabel: UILabel!
     @IBOutlet var GoodButton: UIButton!
+    var GoodPunchViewController : GoodPunchViewController! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         GoodLabel.text = "What did the gorilla call the donkey"
     }
-    
-    @IBAction func GoodButton(_ sender: UIButton) {
-        GoodLabel.text = "donkey"
+
+
+   @IBAction func GoodButton(_ sender: UIButton) {
+        //present(PunPunchViewController(), animated: true)
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        GoodLabel.text = ""
+        GoodPunchViewControllerBuilder()
+        UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+        GoodPunchViewController.view.frame = view.frame
+        switchViewController(from: self, to:GoodPunchViewController)
+        UIView.commitAnimations()
     }
     
+    func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
+    }
+    
+    fileprivate func GoodPunchViewControllerBuilder(){
+        if(GoodPunchViewController == nil) {
+            GoodPunchViewController = (storyboard?.instantiateViewController(withIdentifier: "GoodPunchID") as! GoodPunchViewController)
+        }
+    }
     
 }
 
@@ -28,6 +52,7 @@ class PunViewController: UIViewController {
     
     @IBOutlet var PunLabel: UILabel!
     @IBOutlet var PunButton: UIButton!
+    var PunPunchViewController : PunPunchViewController! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,41 +61,73 @@ class PunViewController: UIViewController {
 
 
    @IBAction func PunButton(_ sender: UIButton) {
-        present(PunPunchViewController(), animated: true)
+        //present(PunPunchViewController(), animated: true)
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        PunLabel.text = ""
+        PunPunchViewControllerBuilder()
+        UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+        PunPunchViewController.view.frame = view.frame
+        switchViewController(from: self, to:PunPunchViewController)
+        UIView.commitAnimations()
+    }
+    
+    func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
+    }
+    
+    fileprivate func PunPunchViewControllerBuilder(){
+        if(PunPunchViewController == nil) {
+            PunPunchViewController = (storyboard?.instantiateViewController(withIdentifier: "PunPunchID") as! PunPunchViewController)
+        }
     }
     
 }
-
-class PunPunchViewController: UIViewController {
-    
-    @IBOutlet var PunPunchLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
-        //PunPunchLabel.text = ""
-    }
-
-    
-}
-
 
 
 class DadViewController: UIViewController {
     
     @IBOutlet var DadLabel: UILabel!
     @IBOutlet var DadButton: UIButton!
+    var DadPunchViewController : DadPunchViewController! = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
         DadLabel.text = "What did the gorilla call the donkey"
     }
-    
-    @IBAction func DadButton(_ sender: UIButton) {
-        DadLabel.text = "Hi"
+
+
+   @IBAction func DadButton(_ sender: UIButton) {
+        //present(PunPunchViewController(), animated: true)
+        UIView.beginAnimations("View Flip", context: nil)
+        UIView.setAnimationDuration(0.4)
+        UIView.setAnimationCurve(.easeInOut)
+        DadLabel.text = ""
+        DadPunchViewControllerBuilder()
+        UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+        DadPunchViewController.view.frame = view.frame
+        switchViewController(from: self, to:DadPunchViewController)
+        UIView.commitAnimations()
     }
     
+    func switchViewController(from: UIViewController?, to: UIViewController?) {
+        if to != nil {
+            self.addChild(to!)
+            self.view.insertSubview(to!.view, at: 0)
+            to!.didMove(toParent: self)
+        }
+    }
     
+    fileprivate func DadPunchViewControllerBuilder(){
+        if(DadPunchViewController == nil) {
+            DadPunchViewController = (storyboard?.instantiateViewController(withIdentifier: "DadPunchID") as! DadPunchViewController)
+        }
+    }
     
 }
 
